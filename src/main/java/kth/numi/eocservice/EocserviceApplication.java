8f9ded34b4c82c54dab1862bfdc9ed895a9b253e
Nuh <92259810+numoh@users.noreply.github.com>
2023-11-25 23:11:24 +0100

    initial commit

---
+++ src/main/java/kth/numi/eocservice/EocserviceApplication.java
@@ -0,0 +1,13 @@
+package kth.numi.eocservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EocserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EocserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kth/numi/eocservice/EocserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package kth.numi.eocservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EocserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
